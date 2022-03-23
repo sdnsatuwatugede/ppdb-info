bdb1bbccdcaabad2b82c424646148ff46e827305
sdnsatuwatugede <sdnwatugede.satu@gmail.com>
2022-03-23 14:10:53 +0700

    main

--- pages/index.js
+++ pages/index.js
@@ -21,31 +21,17 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="#" className={styles.card}>
             <h2>Dokumentasi &rarr;</h2>
             <p>Untuk Pelaksanaan PPDB tahun 2022 di Akhir Bulan Maret</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" className={styles.card}>
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
-        <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
-          Powered by{" "}
+        <a href="https://sdnwatugede.web.id" target="_blank" rel="noopener noreferrer">
+          Powered by{"panitia PPDB SD Negeri 1 Watugede"}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
